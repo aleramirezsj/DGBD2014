907e176d17357eaa0267aa492dbe6df918130c7e
aleramirezsj <aleramirezsj@gmail.com>
2014-05-26 17:45:13 -0300

    Elaboración de una clase que administre la aplicación, pinte la pantalla pida datos de la empresa

--- EjerciosPruebasEnClase/ProgramacionOrientadaAObjetos/Program.cs
+++ EjerciosPruebasEnClase/ProgramacionOrientadaAObjetos/Program.cs
@@ -9,50 +9,8 @@ namespace ProgramacionOrientadaAObjetos
     {
         static void Main(string[] args)
         {
-            //creamos el objeto mipersona basado en la
-            //clase persona
-            Persona mipersona = new Persona();
-            int opcion = 0;
-            //Muestro el menú
-            while (opcion != 4)
-            {
-                Console.Clear();
-                Console.WriteLine("MENU DE OPCIONES");
-                Console.WriteLine("1- Carga de datos");
-                Console.WriteLine("2- Imprimir Ficha de datos");
-                Console.WriteLine("3- Imprimir Datos en un renglón");
-                Console.WriteLine("4- Salir");
-                Console.Write("Ingrese su opción:");
-                try
-                {
-                    opcion = Convert.ToInt32(Console.ReadLine());
-                }
-                catch
-                {
-                    Console.WriteLine("Error... Ingrese una opción correcta");
-                    Console.ReadKey();
-                }
-                switch (opcion)
-                {
-                    case 1:
-                        mipersona.CargarDatos();
-                        break;
-                    case 2:
-                        mipersona.ImprimirFicha();
-                        break;
-                    case 3:
-                        mipersona.ImprimirRenglon();
-                        break;
-                    case 4:
-                        break;
-                    default:
-                        Console.WriteLine("Error... Ingrese una opción correcta");
-                        Console.ReadKey();
-                        break;
-                }
-            }
-            Console.WriteLine("Pulse una tecla para salir del programa");
-            Console.ReadKey();
+            SistemaClientes miSistema = new SistemaClientes();
+
         }
     }
 }
---
+++ EjerciosPruebasEnClase/ProgramacionOrientadaAObjetos/SistemaClientes.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgramacionOrientadaAObjetos
+{
+    class SistemaClientes
+    {
+        public Persona cliente;
+        private String tituloEmpresa;
+        private String DatosEmpresa;
+
+        public SistemaClientes()
+        {
+            InicioEmpresa();
+            cliente = new Persona();
+            MostrarMenu();
+            
+        }
+
+        public void InicioEmpresa()
+        {
+            Console.WriteLine("Carga de datos de la empresa");
+            Console.Write("Ingrese el nombre:");
+            tituloEmpresa = Console.ReadLine();
+            Console.Write("Ingrese datos de la empresa (TE, Dirección, Email):");
+            DatosEmpresa = Console.ReadLine();
+        }
+
+        public void MostrarMenu()
+        {
+            int opcion = 0;
+            //Muestro el menú
+            while (opcion != 4)
+            {
+                Console.Clear();
+                Console.WriteLine("SISTEMA DE CLIENTES :: "+tituloEmpresa);
+                Console.WriteLine("1- Carga de cliente");
+                Console.WriteLine("2- Imprimir Ficha de cliente");
+                Console.WriteLine("3- Imprimir cliente en un renglón");
+                Console.WriteLine("4- Salir");
+                Console.Write("Ingrese su opción:");
+                try
+                {
+                    opcion = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Error... Ingrese una opción correcta (Pulse cualquier tecla para continuar)");
+                    Console.ReadKey();
+                    continue;
+                }
+                switch (opcion)
+                {
+                    case 1:
+                        cliente.CargarDatos();
+                        break;
+                    case 2:
+                        cliente.ImprimirFicha();
+                        break;
+                    case 3:
+                        cliente.ImprimirRenglon();
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        Console.WriteLine("Error... Ingrese una opción correcta (Pulse cualquier tecla para continuar)");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+            Console.WriteLine("Pulse una tecla para salir del programa");
+            Console.ReadKey();
+        }
+    }
+}
